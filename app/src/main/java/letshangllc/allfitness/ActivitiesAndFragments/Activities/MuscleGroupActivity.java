7d311422a75cd9e12d7150294f36e06bf58551e7
Chase Burnham <cburnhamiv@gmail.com>
2016-05-11 06:56:28 -0400

    Added routine activity

---
+++ app/src/main/java/letshangllc/allfitness/ActivitiesAndFragments/Activities/MuscleGroupActivity.java
@@ -0,0 +1,15 @@
+package letshangllc.allfitness.ActivitiesAndFragments.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import letshangllc.allfitness.R;
+
+public class MuscleGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_muscle_group);
+    }
+}
