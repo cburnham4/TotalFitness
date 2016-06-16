39f50e8c7e00a3520b7d8ecabbb847d7e1f9f5fe
Chase Burnham <cburnhamiv@gmail.com>
2016-06-15 21:32:16 -0400

    Added cardio ad set layout

--- app/src/main/java/letshangllc/allfitness/ActivitiesAndFragments/typefragments/cardio/AddCardioSetFragment.java
+++ app/src/main/java/letshangllc/allfitness/ActivitiesAndFragments/typefragments/cardio/AddCardioSetFragment.java
@@ -77,9 +77,9 @@ public class AddCardioSetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_add_lift_set, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_cardio_set, container, false);
 
-        this.setupViews(view);
+        //this.setupViews(view);
 
 
         return view;
